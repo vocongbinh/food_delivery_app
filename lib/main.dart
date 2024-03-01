db0930c4ff5012872ebeb60a9888cf35f95250e8
vocongbinh <binh93phudong@gmail.com>
2024-03-01 12:55:44 +0700

    create assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/values/app_assets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -112,6 +113,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            TextButton(
+                onPressed: () {}, child: Image.asset(AppAssets.backIcons))
           ],
         ),
       ),
---
+++ lib/values/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String images = 'assets/images/';
+  static const String fonts = 'assets/fonts/';
+  static const String icons = 'assets/icons/';
+  static const String backIcons = '${icons}back_icon.png';
+  static const String menuIcons = '${icons}menu_icon.png';
+}
